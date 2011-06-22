b97e715840324f0facbd3696612d5f7f668ff149
Nando Vieira <fnando.vieira@gmail.com>
2011-06-22 13:52:13 -0300

    Bump up version

--- lib/redis/settings/version.rb
+++ lib/redis/settings/version.rb
@@ -2,8 +2,8 @@ class Redis
   class Settings
     module Version
       MAJOR = 0
-      MINOR = 1
-      PATCH = 1
+      MINOR = 2
+      PATCH = 0
       STRING = "#{MAJOR}.#{MINOR}.#{PATCH}"
     end
   end
